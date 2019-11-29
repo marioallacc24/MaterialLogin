f4ddbb021e198b01158095265665ab13f629338d
EliteBook <EliteBook@ELITEBOOK>
2019-11-29 14:53:39 +0100

    Dodavanje klase za rad sa bazom i klase user

---
+++ MaterialLogin/MaterialLogin/Class/SqlDataAcess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialLogin.Class
+{
+    class SqlDataAcess
+    {
+
+
+        public SqlDataAcess()
+        {
+
+        }
+
+
+        public bool AddUser()
+        {
+            return true;
+        }
+
+        public bool EditUser()
+        {
+            return true;
+        }
+
+        public bool DeleteUser()
+        {
+            return true;
+        }
+
+        public User ReadUser()
+        {
+            return null;
+        }
+
+
+
+
+    }
+}
